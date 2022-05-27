e65619c45889ca43d6d60d6ef4db64603f6be8fb
Nisan <nnisan858@gmail.com>
2022-05-27 14:33:57 +0300

    adding edit & remove icons

--- todo_app/dist/script.js
+++ todo_app/dist/script.js
@@ -10,11 +10,19 @@ function loadListFromLocalStorage(listArea) {
 function createNewTaskElement(newTask) {
     const newTaskElement = document.createElement('div');
     const checkBox = document.createElement('input');
+    const deletIcon = document.createElement('i');
+    deletIcon.classList.add('material-icons', 'delete-icon');
+    deletIcon.innerText = "delete_forever";
+    const editIcon = document.createElement('i');
+    editIcon.classList.add('material-icons', "edit-icon");
+    editIcon.innerText = "edit_note";
     checkBox.type = 'checkBox';
     const text = document.createElement('p');
     text.innerHTML = newTask.name;
     newTaskElement.append(checkBox);
     newTaskElement.append(text);
+    newTaskElement.append(editIcon);
+    newTaskElement.append(deletIcon);
     newTaskElement.classList.add('task');
     return newTaskElement;
 }
