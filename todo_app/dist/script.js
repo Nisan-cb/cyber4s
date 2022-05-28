1f4f533bd6352e1d57a16f158a59ad7c78643c29
Nisan <nnisan858@gmail.com>
2022-05-28 22:45:48 +0300

    addTask & deleteTask functions

--- todo_app/dist/script.js
+++ todo_app/dist/script.js
@@ -1,71 +1,82 @@
 "use strict";
 console.log("TODO App");
-const todoList = []; // array of tasks
-function loadListFromLocalStorage(listArea) {
-    if (!localStorage.todoList)
-        return;
-    for (const task of JSON.parse(localStorage.todoList)) {
-        todoList.push(task);
-        listArea.append(createNewTaskElement(task));
+class TaskManager {
+    constructor(listAreaElement) {
+        this.listAreaElemet = listAreaElement;
+        this.taskArray = [];
     }
-}
-function deleteTask(e, task) {
-    todoList.splice(todoList.indexOf(task), 1); // remove from array
-    localStorage.setItem('todoList', JSON.stringify(todoList)); // update the local storage
-    e.target.parentNode.remove(); // remove the HTML element
-}
-function editTask(e, task) {
-    console.log("edit mode");
-}
-function checkTask(e, task) {
-    task.isDone = !task.isDone;
-    if (task.isDone)
-        e.target.parentNode.classList.add("done");
-    else
-        e.target.parentNode.classList.remove("done");
-    localStorage.setItem('todoList', JSON.stringify(todoList)); // update the local storage
-}
-function createNewTaskElement(newTask) {
-    const newTaskElement = document.createElement('div');
-    // newTaskElement.id = `task-${newTask.id}`
-    const checkBox = document.createElement('input');
-    const deletIcon = document.createElement('i');
-    deletIcon.classList.add('material-icons', 'delete-icon');
-    deletIcon.innerText = "delete_forever";
-    const editIcon = document.createElement('i');
-    editIcon.classList.add('material-icons', "edit-icon");
-    editIcon.innerText = "edit_note";
-    checkBox.type = 'checkBox';
-    const text = document.createElement('p');
-    text.innerHTML = newTask.name;
-    newTaskElement.append(checkBox);
-    newTaskElement.append(text);
-    newTaskElement.append(editIcon);
-    newTaskElement.append(deletIcon);
-    newTaskElement.classList.add('task');
-    if (newTask.isDone)
-        newTaskElement.classList.add('done');
-    deletIcon.addEventListener('click', (e) => { deleteTask(e, newTask); });
-    editIcon.addEventListener('click', (e) => { editTask(e, newTask); });
-    checkBox.addEventListener('change', (e) => { checkTask(e, newTask); });
-    return newTaskElement;
+    lenght() {
+        return this.taskArray.length;
+    }
+    loadTasksFromLoclStorage() {
+        if (!localStorage.todoList)
+            return null;
+        for (const task of JSON.parse(localStorage.todoList))
+            this.addNewTask(task.content, task.isDone);
+        return this.taskArray;
+    }
+    addNewTask(content, mode = false) {
+        const task = {
+            content: content,
+            isDone: mode
+        };
+        console.log(task);
+        this.taskArray.push(task);
+        const newTaskElement = document.createElement('div');
+        const checkBox = document.createElement('input');
+        const deletIcon = document.createElement('i');
+        const editIcon = document.createElement('i');
+        const text = document.createElement('p');
+        deletIcon.classList.add("material-icons", "delete-icon");
+        deletIcon.innerHTML = "delete";
+        editIcon.classList.add("material-icons", "edit-icon");
+        editIcon.innerHTML = "edit";
+        newTaskElement.classList.add('task');
+        checkBox.type = 'checkbox';
+        text.textContent = task.content;
+        if (task.isDone) {
+            text.classList.add('done');
+            checkBox.checked = true;
+        }
+        deletIcon.addEventListener('click', () => { this.deleteTask(task, newTaskElement); });
+        editIcon.addEventListener('click', this.editTask);
+        checkBox.addEventListener('change', () => { this.checked(task, text); });
+        newTaskElement.append(checkBox);
+        newTaskElement.append(text);
+        newTaskElement.append(editIcon);
+        newTaskElement.append(deletIcon);
+        this.listAreaElemet.insertBefore(newTaskElement, this.listAreaElemet.firstChild);
+        // this.listAreaElemet.append(newTaskElement)
+    }
+    checked(task, textElement) {
+        task.isDone = !task.isDone;
+        if (task.isDone)
+            textElement.classList.add('done');
+        else
+            textElement.classList.remove('done');
+        localStorage.setItem('todoList', JSON.stringify(this.taskArray));
+    }
+    deleteTask(task, taskElement) {
+        console.log("delete");
+        this.taskArray.splice(this.taskArray.indexOf(task), 1);
+        taskElement.remove();
+        localStorage.setItem('todoList', JSON.stringify(this.taskArray));
+    }
+    editTask() {
+        console.log("edit mode");
+    }
+    saveChanges() {
+        console.log("saved");
+    }
 }
 window.addEventListener('load', () => {
     const addBtn = document.getElementById('add-btn');
     const input = document.getElementById('input');
     const listArea = document.getElementById('list-area');
-    loadListFromLocalStorage(listArea);
+    const taskManager = new TaskManager(listArea);
+    taskManager.loadTasksFromLoclStorage();
     addBtn?.addEventListener('click', () => {
-        console.log("added");
-        console.log(input?.value);
-        let newTask = {
-            name: String(input?.value),
-            isDone: false,
-            id: todoList.length
-        };
-        todoList.push(newTask); //adding the new task to the list
-        localStorage.todoList = JSON.stringify(todoList);
-        let newTaskElement = createNewTaskElement(newTask);
-        listArea?.append(newTaskElement);
+        taskManager.addNewTask(input.value);
+        localStorage.setItem('todoList', JSON.stringify(taskManager.taskArray));
     });
 });
--- todo_app/script.ts
+++ todo_app/script.ts
@@ -1,70 +1,94 @@
 console.log("TODO App");
 
 interface Task {
-    name: string,
-    isDone: boolean,
-    id: number
+    content: string,
+    isDone: boolean
 }
 
+class TaskManager {
+    taskArray: Task[];  // array of tasks
+    listAreaElemet: HTMLElement;
+    constructor(listAreaElement: HTMLElement) {
+        this.listAreaElemet = listAreaElement;
+        this.taskArray = [];
+    }
 
+    lenght(): Number {
+        return this.taskArray.length;
+    }
 
-const todoList: Task[] = [];  // array of tasks
+    loadTasksFromLoclStorage(): Task[] | null {    // load tasks from local storege
+        if (!localStorage.todoList) return null;
+        for (const task of JSON.parse(localStorage.todoList))
+            this.addNewTask(task.content, task.isDone)
+        return this.taskArray;
+    }
 
-function loadListFromLocalStorage(listArea: HTMLElement) {
-    if (!localStorage.todoList) return;
-    for (const task of JSON.parse(localStorage.todoList)) {
-        todoList.push(task);
-        listArea.append(createNewTaskElement(task));
+    addNewTask(content: string, mode: boolean = false) {
+        const task: Task = {
+            content: content,
+            isDone: mode
+        };
+        console.log(task);
+
+        this.taskArray.push(task);
+        const newTaskElement = document.createElement('div');
+        const checkBox = document.createElement('input');
+        const deletIcon = document.createElement('i');
+        const editIcon = document.createElement('i');
+        const text = document.createElement('p');
+
+        deletIcon.classList.add("material-icons", "delete-icon");
+        deletIcon.innerHTML = "delete";
+        editIcon.classList.add("material-icons", "edit-icon");
+        editIcon.innerHTML = "edit";
+        newTaskElement.classList.add('task')
+        checkBox.type = 'checkbox';
+        text.textContent = task.content;
+
+        if (task.isDone) {
+            text.classList.add('done');
+            checkBox.checked = true;
+        }
+        deletIcon.addEventListener('click', () => { this.deleteTask(task, newTaskElement) });
+        editIcon.addEventListener('click', this.editTask);
+        checkBox.addEventListener('change', () => { this.checked(task, text) });
+
+        newTaskElement.append(checkBox);
+        newTaskElement.append(text);
+        newTaskElement.append(editIcon);
+        newTaskElement.append(deletIcon);
+
+        this.listAreaElemet.insertBefore(newTaskElement, this.listAreaElemet.firstChild)
+        // this.listAreaElemet.append(newTaskElement)
+    }
+
+    checked(task: Task, textElement: HTMLParagraphElement) {
+        task.isDone = !task.isDone;
+        if (task.isDone)
+            textElement.classList.add('done');
+        else
+            textElement.classList.remove('done');
+
+        localStorage.setItem('todoList', JSON.stringify(this.taskArray));
+    }
+
+    deleteTask(task: Task, taskElement: HTMLDivElement) {
+        console.log("delete")
+        this.taskArray.splice(this.taskArray.indexOf(task), 1);
+        taskElement.remove();
+        localStorage.setItem('todoList', JSON.stringify(this.taskArray));
+    }
+    editTask() {
+        console.log("edit mode")
+    }
+
+    saveChanges() {
+        console.log("saved");
     }
 }
-function deleteTask(e: Event, task: Task) {
-    todoList.splice(todoList.indexOf(task), 1); // remove from array
-    localStorage.setItem('todoList', JSON.stringify(todoList)); // update the local storage
-    (e.target as any).parentNode.remove();  // remove the HTML element
-}
 
-function editTask(e: Event, task: Task) {
-    console.log("edit mode");
-}
 
-function checkTask(e: Event, task: Task) {
-    task.isDone = !task.isDone;
-    if (task.isDone)
-        (e.target as any).parentNode.classList.add("done");
-    else
-        (e.target as any).parentNode.classList.remove("done");
-    localStorage.setItem('todoList', JSON.stringify(todoList)); // update the local storage
-}
-
-function createNewTaskElement(newTask: Task): HTMLElement {
-    const newTaskElement = document.createElement('div');
-    // newTaskElement.id = `task-${newTask.id}`
-    const checkBox = document.createElement('input');
-    const deletIcon = document.createElement('i');
-    deletIcon.classList.add('material-icons', 'delete-icon');
-    deletIcon.innerText = "delete_forever";
-
-    const editIcon = document.createElement('i');
-    editIcon.classList.add('material-icons', "edit-icon");
-    editIcon.innerText = "edit_note";
-
-    checkBox.type = 'checkBox'
-    const text = document.createElement('p');
-    text.innerHTML = newTask.name;
-    newTaskElement.append(checkBox);
-    newTaskElement.append(text);
-    newTaskElement.append(editIcon);
-    newTaskElement.append(deletIcon);
-    newTaskElement.classList.add('task');
-
-    if (newTask.isDone)
-        newTaskElement.classList.add('done');
-
-    deletIcon.addEventListener('click', (e) => { deleteTask(e, newTask) });
-    editIcon.addEventListener('click', (e) => { editTask(e, newTask) });
-    checkBox.addEventListener('change', (e) => { checkTask(e, newTask) })
-    return newTaskElement;
-}
 
 
 
@@ -74,19 +98,12 @@ window.addEventListener('load', () => {
     const addBtn: HTMLElement = document.getElementById('add-btn') as HTMLElement;
     const input: HTMLInputElement = document.getElementById('input') as HTMLInputElement;
     const listArea = document.getElementById('list-area') as HTMLElement;
-    loadListFromLocalStorage(listArea);
+
+    const taskManager: TaskManager = new TaskManager(listArea);
+    taskManager.loadTasksFromLoclStorage();
 
     addBtn?.addEventListener('click', () => {
-        console.log("added");
-        console.log(input?.value);
-        let newTask: Task = {   // creating new task object
-            name: String(input?.value),
-            isDone: false,
-            id: todoList.length
-        }
-        todoList.push(newTask)  //adding the new task to the list
-        localStorage.todoList = JSON.stringify(todoList);
-        let newTaskElement: HTMLElement = createNewTaskElement(newTask);
-        listArea?.append(newTaskElement);
+        taskManager.addNewTask(input.value)
+        localStorage.setItem('todoList', JSON.stringify(taskManager.taskArray));
     });
 })
