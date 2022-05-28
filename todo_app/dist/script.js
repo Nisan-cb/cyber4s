e684a0c7a9c81e374b67fc5baf82701b700c5bd3
Nisan <nnisan858@gmail.com>
2022-05-29 02:56:51 +0300

    drag and drop

--- todo_app/dist/script.js
+++ todo_app/dist/script.js
@@ -11,17 +11,43 @@ class TaskManager {
     loadTasksFromLoclStorage() {
         if (!localStorage.todoList)
             return null;
-        for (const task of JSON.parse(localStorage.todoList))
-            this.addNewTask(task.content, task.isDone);
+        for (const [i, task] of JSON.parse(localStorage.todoList).entries())
+            this.addNewTask(task.content, i, task.isDone);
         return this.taskArray;
     }
-    addNewTask(content, mode = false) {
+    addNewTask(content, id, mode = false) {
         const task = {
             content: content,
             isDone: mode
+            // id: `task-${id}`
         };
         this.taskArray.push(task);
         const newTaskElement = document.createElement('div');
+        newTaskElement.draggable = true;
+        newTaskElement.id = String(id);
+        newTaskElement.addEventListener('dragstart', (e) => {
+            console.log(e.target);
+            e.dataTransfer?.setData('text/plain', e.target.id);
+            setTimeout(() => {
+                newTaskElement.style.display = "none";
+            }, 0);
+        });
+        // newTaskElement.addEventListener('dragover', (e) => {
+        //     e.preventDefault();
+        //     e.stopPropagation();
+        // })
+        newTaskElement.addEventListener('drop', (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            const id = e.dataTransfer?.getData('text/plain');
+            console.log(id);
+            let draggableElement = document.getElementById(id);
+            console.log(draggableElement);
+            this.listAreaElemet.insertBefore(draggableElement, e.target);
+            // (e.target as HTMLElement).parentNode?.appendChild(draggableElement as Node);
+            draggableElement.style.display = 'flex';
+            this.swapId(Number(draggableElement.id), Number(e.target.id));
+        });
         const checkBox = document.createElement('input');
         const deletIcon = document.createElement('i');
         const editIcon = document.createElement('i');
@@ -47,6 +73,24 @@ class TaskManager {
         newTaskElement.append(editIcon);
         newTaskElement.append(deletIcon);
         this.listAreaElemet.insertBefore(newTaskElement, this.listAreaElemet.firstChild);
+    }
+    swapId(index1, index2) {
+        const task = this.taskArray[index1];
+        if (index1 > index2) {
+            for (let i = index1 - 1; i > index2; i--) {
+                this.taskArray[i + 1] = this.taskArray[i];
+            }
+            this.taskArray[index2 + 1] = task;
+        }
+        else {
+            for (let i = index1 + 1; i <= index2; i++) {
+                this.taskArray[i - 1] = this.taskArray[i];
+            }
+            this.taskArray[index2] = task;
+        }
+        // this.taskArray[index1] = this.taskArray[index2];
+        console.log(this.taskArray);
+        localStorage.setItem('todoList', JSON.stringify(this.taskArray));
     }
     checked(task, textElement) {
         task.isDone = !task.isDone;
@@ -98,7 +142,7 @@ window.addEventListener('load', () => {
     addBtn?.addEventListener('click', () => {
         if (!input.value.trim().length)
             return;
-        taskManager.addNewTask(input.value);
+        taskManager.addNewTask(input.value, taskManager.taskArray.length);
         localStorage.setItem('todoList', JSON.stringify(taskManager.taskArray));
         input.value = "";
     });
@@ -108,4 +152,27 @@ window.addEventListener('load', () => {
             addBtn.click();
         }
     });
+    // listArea.addEventListener('dragenter', (e) => {
+    //     e.preventDefault();
+    //     console.log("ok");
+    //     // listArea.style.cursor = 'pointer'
+    // })
+    listArea.addEventListener('drop', (e) => {
+        const id = e.dataTransfer?.getData('text/plain');
+        // console.log("leaved")
+        // console.log(id);
+        let draggableElement = document.getElementById(id);
+        draggableElement.style.display = 'flex';
+    });
+    // listArea.addEventListener('dragleave', (e) => {
+    //     const id = e.dataTransfer?.getData('text/plain');
+    //     // console.log("leaved")
+    //     // console.log(id);
+    //     let draggableElement = document.getElementById(id as string);
+    //     (draggableElement as HTMLElement).style.display = 'flex';
+    // })
+    listArea.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }, true);
 });
--- todo_app/script.ts
+++ todo_app/script.ts
@@ -3,6 +3,7 @@ console.log("TODO App");
 interface Task {
     content: string,
     isDone: boolean
+    // id: string
 }
 
 class TaskManager {
@@ -19,18 +20,48 @@ class TaskManager {
 
     loadTasksFromLoclStorage(): Task[] | null {    // load tasks from local storege
         if (!localStorage.todoList) return null;
-        for (const task of JSON.parse(localStorage.todoList))
-            this.addNewTask(task.content, task.isDone)
+
+        for (const [i, task] of JSON.parse(localStorage.todoList).entries())
+            this.addNewTask(task.content, i, task.isDone)
         return this.taskArray;
     }
 
-    addNewTask(content: string, mode: boolean = false) {
+    addNewTask(content: string, id: number, mode: boolean = false) {
         const task: Task = {
             content: content,
             isDone: mode
+            // id: `task-${id}`
         };
         this.taskArray.push(task);
         const newTaskElement = document.createElement('div');
+        newTaskElement.draggable = true;
+        newTaskElement.id = String(id);
+        newTaskElement.addEventListener('dragstart', (e) => {
+            console.log(e.target)
+            e.dataTransfer?.setData('text/plain', (e.target as HTMLElement).id);
+            setTimeout(() => {
+                newTaskElement.style.display = "none";
+            }, 0);
+        })
+        // newTaskElement.addEventListener('dragover', (e) => {
+        //     e.preventDefault();
+        //     e.stopPropagation();
+        // })
+
+        newTaskElement.addEventListener('drop', (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+
+            const id = e.dataTransfer?.getData('text/plain');
+            console.log(id);
+            let draggableElement = document.getElementById(id as string);
+            console.log(draggableElement);
+            this.listAreaElemet.insertBefore(draggableElement as HTMLElement, e.target as HTMLElement);
+            // (e.target as HTMLElement).parentNode?.appendChild(draggableElement as Node);
+            (draggableElement as HTMLElement).style.display = 'flex';
+            this.swapId(Number((draggableElement as HTMLElement).id), Number((e.target as HTMLElement).id));
+        })
+
         const checkBox = document.createElement('input');
         const deletIcon = document.createElement('i');
         const editIcon = document.createElement('i');
@@ -60,6 +91,27 @@ class TaskManager {
         newTaskElement.append(deletIcon);
 
         this.listAreaElemet.insertBefore(newTaskElement, this.listAreaElemet.firstChild)
+    }
+
+    swapId(index1: number, index2: number) {
+
+        const task = this.taskArray[index1];
+        if (index1 > index2) {
+            for (let i = index1 - 1; i > index2; i--) {
+                this.taskArray[i + 1] = this.taskArray[i];
+            }
+            this.taskArray[index2 + 1] = task;
+        } else {
+            for (let i = index1 + 1; i <= index2; i++) {
+                this.taskArray[i - 1] = this.taskArray[i];
+            }
+            this.taskArray[index2] = task;
+        }
+        // this.taskArray[index1] = this.taskArray[index2];
+        console.log(this.taskArray)
+        localStorage.setItem('todoList', JSON.stringify(this.taskArray));
+
+
     }
 
     checked(task: Task, textElement: HTMLParagraphElement) {
@@ -124,7 +176,7 @@ window.addEventListener('load', () => {
 
     addBtn?.addEventListener('click', () => {
         if (!input.value.trim().length) return;
-        taskManager.addNewTask(input.value)
+        taskManager.addNewTask(input.value, taskManager.taskArray.length)
         localStorage.setItem('todoList', JSON.stringify(taskManager.taskArray));
         input.value = "";
     });
@@ -134,4 +186,34 @@ window.addEventListener('load', () => {
             addBtn.click();
         }
     })
+
+    // listArea.addEventListener('dragenter', (e) => {
+    //     e.preventDefault();
+    //     console.log("ok");
+    //     // listArea.style.cursor = 'pointer'
+    // })
+    listArea.addEventListener('drop', (e) => {
+        const id = e.dataTransfer?.getData('text/plain');
+        // console.log("leaved")
+        // console.log(id);
+        let draggableElement = document.getElementById(id as string);
+
+        (draggableElement as HTMLElement).style.display = 'flex';
+
+    })
+    // listArea.addEventListener('dragleave', (e) => {
+    //     const id = e.dataTransfer?.getData('text/plain');
+    //     // console.log("leaved")
+    //     // console.log(id);
+    //     let draggableElement = document.getElementById(id as string);
+
+    //     (draggableElement as HTMLElement).style.display = 'flex';
+
+    // })
+
+    listArea.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }, true)
+
 })
